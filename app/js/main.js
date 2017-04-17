9e72fa2ce9d2598a931d70ccc545b1ab52cdd1fe
Daniel Ge <daniel@dge.io>
2017-04-16 19:08:49 -0700

    Add SCSS into Webpack build pipeline

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
+// At first glance, requiring an SCSS file in a JS file looks very wrong. But
+// here's how this works.
+//
+// Our webpack.config.js declares that SCSS will be compiled into CSS
+// (sass-loader). The CSS will then be converted into a CommonJS module
+// (css-loader); this means that it can be required and that it will get
+// injected into the document as a <style> tag. The ExtractTextPlugin then
+// extracts this style blob from the intermediate generated JS bundle into a
+// bundle.css that gets served separately.
+//
+// Don't you love frontend development?
+require('../css/main.scss');
+
 import WheelMediator from './wheel';
 
 function getRandomIntInclusive(min, max) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,45 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractScss = new ExtractTextPlugin('bundle.css');
 
 module.exports = {
+  // Generate source maps
+  // TODO: disable when generating for production
+  devtool: 'source-map',
+
   // Configuration for the webpack-dev-server
   devServer: {
     // Webpack bundled assets will be found at [server]/dist/
     publicPath: '/dist/'
   },
+
+  // Webpack starts here in JS land
   entry: './app/js/main.js',
+
+  module: {
+    rules: [{
+      // SCSS compilation rules (requires sass-loader, css-loader)
+      test: /\.scss$/,
+
+      // Source maps should be specified for both loaders
+      // Normally, styles will get compiled into JS and injected via <style>
+      // tag. That's full of silliness, so we will extract them into a separate
+      // file.
+      loader: extractScss.extract({
+        use: [{
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "sass-loader",
+          options: { sourceMap: true }
+        }]
+      })
+    }]
+  },
+  plugins: [
+    extractScss
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
