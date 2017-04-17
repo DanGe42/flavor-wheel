ae497a78e426005a0733f78499e6bf492c1b9c7a
Daniel Ge <daniel@dge.io>
2017-04-16 19:07:11 -0700

    Serve bundled assets /dist/ in the dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 var path = require('path');
 
 module.exports = {
+  // Configuration for the webpack-dev-server
+  devServer: {
+    // Webpack bundled assets will be found at [server]/dist/
+    publicPath: '/dist/'
+  },
   entry: './app/js/main.js',
   output: {
     filename: 'bundle.js',
